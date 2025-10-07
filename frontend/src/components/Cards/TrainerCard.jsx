84bb349cc5071122a2f41ec99e251ccaf3c37276
Rasan Samarakkody <rasansamarakkody077@gmail.com>
2025-10-08 01:41:30 +0530

    do

--- frontend/src/components/Cards/TrainerCard.jsx
+++ frontend/src/components/Cards/TrainerCard.jsx
@@ -64,7 +64,7 @@ const TrainerCard = ({ trainer, onViewDetails }) => {
         <img
           src={trainer.image || '/images/default-trainer.png'}
           alt={trainer.trainerName}
-          className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-110"
+          className="w-full h-64 sm:h-72 md:h-64 lg:h-72 object-cover transition-transform duration-500 group-hover:scale-110"
         />
         
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
